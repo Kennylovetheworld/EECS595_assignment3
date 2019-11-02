ec3ce9e140907d6c90a2119cc46e672e2921603f
Kennylovetheworld <944545895@qq.com>
2019-11-02 17:21:17 -0400

    /Users/fanshukai/anaconda3/envs/nlp/bin/python /Users/fanshukai/Desktop/EECS595_assignment3/assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -5,21 +5,21 @@ from nltk import word_tokenize
 def CNF(prob, root, rhs_list):
     rules = []
     # find rhs1 amd rhs2
-    rhs1 = rhs_list[0]
+    rhs2 = rhs_list[-1]
     if len(rhs_list) == 2:
-        rhs2 = rhs_list[1]
+        rhs1 = rhs_list[0]
         rule = Rule(prob, root, rhs1, rhs2)
         rules.append(rule)
     else:
-        rhs2 = '-' 
-        for item in rhs_list[1:]:
-            rhs2 = rhs2 + item + '-'
+        rhs1 = '-' 
+        for item in rhs_list[:-1]:
+            rhs1 = rhs1 + item + '-'
         # construct a rule
         rule = Rule(prob, root, rhs1, rhs2)
         rules.append(rule)
         #recurrsion
         if CNF(1, rhs2, rhs_list[1:]) is not None:
-            rules.extend(CNF(1, rhs2, rhs_list[1:]))
+            rules.extend(CNF(1, rhs1, rhs_list[:-1]))
     return rules
 
 def CNF_grammer_list(grammer):
@@ -34,8 +34,20 @@ def CNF_grammer_list(grammer):
         if type(production.rhs()[0]) is not nltk.grammar.Nonterminal:
             rule = Rule(production.prob(), str(production.lhs()), str(production.rhs()[0]))
             grammer_list.append(rule)
+    grammer_list = squeeze_grammer_list(grammer_list)
     return grammer_list
 
+def squeeze_grammer_list(grammer_list):
+    new_grammer_list = []
+    delete_list = set()
+    for idx1, item1 in enumerate(grammer_list):
+        for idx2, item2 in enumerate(grammer_list):
+            if idx1 != idx2 and item1 == item2:
+                delete_list.add(min(idx1, idx2))
+    for i in range(len(grammer_list)):
+        if i not in delete_list:
+            new_grammer_list.append(grammer_list[i])
+    return new_grammer_list
 class Rule():
     def __init__(self, prob, root, left, right = None):
         self.root = root # string
@@ -46,6 +58,11 @@ class Rule():
         if self.right == None:
             return self.root + " => (" + self.left + ') [' + str(self.prob) + '] \n'
         return self.root + " => (" + self.left + ' ' + self.right + ') [' + str(self.prob) + '] \n'
+    def __eq__(self, other):
+        if self.root == other.root and self.left == other.left and self.right == other.right and self.prob == other.prob:
+            return True
+        else:
+            return False
 
 class traceback_Rule():
     def __init__(self, rule, prob, traceback = None):
@@ -66,7 +83,6 @@ def CKY(line, grammer_list):
     words = word_tokenize(line)
     if words == []:
         return None
-    print(words)
     table = []
     for i in range(len(words)):
         row = []
@@ -87,7 +103,7 @@ def traceback_table(table, length):
     best_tbrule = None
     best_prob = 0
     #find the best rule
-    for item in table[length-1][length-1]:
+    for item in table[0][length-1]:
         if item.rule.root == 'S' and item.rule.prob > best_prob:
             best_tbrule = item 
             best_prob = item.rule.prob
@@ -104,7 +120,10 @@ def print_tree(tbrule, table):
     #find the corresponding tbrule
     tbrule1 = table[tbrule.traceback.row1][tbrule.traceback.col1][tbrule.traceback.idx1]
     tbrule2 = table[tbrule.traceback.row2][tbrule.traceback.col2][tbrule.traceback.idx2]
-    output = '[' + tbrule.rule.root + ' ' + print_tree(tbrule1) + ' ' + print_tree(tbrule2) + ']'
+    if tbrule.rule.root[0] == '-':
+        output = print_tree(tbrule1, table) + ' ' + print_tree(tbrule2, table)
+    else:
+        output = '[' + tbrule.rule.root + ' ' + print_tree(tbrule1, table) + ' ' + print_tree(tbrule2, table) + ']'
     return output
 
 def find_traceback_rule(table, row, col, sent_length):
@@ -129,8 +148,19 @@ grammer_list = CNF_grammer_list(grammer)
 
 with open('TestingRaw.txt', 'r') as f:
     testlines = f.readlines()
+    pred_results = []
     for line in testlines:
-        table, length = CKY(line, grammer_list)
-        tree = traceback_table(table, length)
+        if word_tokenize(line) == []:
+            tree = ""
+        else:
+            table, length = CKY(line, grammer_list)
+            tree = traceback_table(table, length)
+        pred_results.append(tree)
 
-
+with open('TestingTree.txt', 'r') as f:
+    truetrees = f.readlines()
+    for i, item in enumerate(pred_results):
+        if pred_results[i] == item:
+            print(True)
+        
+    
